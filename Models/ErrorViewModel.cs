04920bdab2874fc6b62700e929f8910d597a6502
farhadrahimov <farhadrahimov@users.noreply.github.com>
2020-05-27 14:54:13 +0400

    ghg

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mstock_27._05._2020_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
